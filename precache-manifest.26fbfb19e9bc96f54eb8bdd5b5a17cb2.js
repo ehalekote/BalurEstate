38b751fa1b5b446a4a15c69c88e8d20c1e8a9a14
Eshan Halekote <ehalekote@gmail.com>
2018-12-25 14:24:46 -0600

    Updates

---
+++ precache-manifest.26fbfb19e9bc96f54eb8bdd5b5a17cb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72c4a205fb1cf277ee9c",
+    "url": "/BalurEstate/static/css/main.b318ccb6.chunk.css"
+  },
+  {
+    "revision": "72c4a205fb1cf277ee9c",
+    "url": "/BalurEstate/static/js/main.72c4a205.chunk.js"
+  },
+  {
+    "revision": "405e360195ba4161eaeb",
+    "url": "/BalurEstate/static/css/1.9f13691c.chunk.css"
+  },
+  {
+    "revision": "405e360195ba4161eaeb",
+    "url": "/BalurEstate/static/js/1.405e3601.chunk.js"
+  },
+  {
+    "revision": "6ddfc8cdd290499a9434",
+    "url": "/BalurEstate/static/js/runtime~main.6ddfc8cd.js"
+  },
+  {
+    "revision": "0e4e359ff7d8bf283f431eaa9edfeeff",
+    "url": "/BalurEstate/index.html"
+  }
+];
