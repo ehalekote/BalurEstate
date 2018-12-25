168bdb3a9ca106ffc34195da0ab3d35851ac0e22
Eshan Halekote <ehalekote@gmail.com>
2018-12-25 14:54:27 -0600

    Switched back to nomal router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link, hashHistory } from "react-router-dom";
-import { HashRouter } from 'react-router-dom'
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
 //Components
@@ -108,7 +106,7 @@ class App extends Component {
     let col1_Accomodation=["/imgs/side_shot_house.jpeg","/imgs/img_1.JPG","/imgs/DSC06939-510x510.jpg"]
     
     return (
-      <HashRouter>
+      <Router>
       <ScrollToTop>
         <div className="App">
         
@@ -155,7 +153,7 @@ class App extends Component {
 
         </div>
         </ScrollToTop>
-      </HashRouter>
+      </Router>
     );
   }
 }
